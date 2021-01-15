1e5d1ca069fc628d97161e17c03b1822b8752ff4
korbindeman@hotmail.com <korbindeman@hotmail.com>
2021-01-15 17:50:00 -0400

    feat: added tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import '@/styles/global.css';
+import 'tailwindcss/tailwind.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return <Component {...pageProps} />;
 }
